16452a324ea9b6aadba3558ea449379d1ba57f7f
Peefy <xpf6677@163.com>
2022-12-29 17:15:30 +0800

    feat: add kcl install scripts.

---
+++ static/script/install.ps1
@@ -0,0 +1,169 @@
+# ------------------------------------------------------------
+# Copyright 2022 The KCL Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Reference: https://github.com/dapr/cli/tree/master/install
+# ------------------------------------------------------------
+param (
+    [string]$Version,
+    [string]$KCLRoot = "$Env:SystemDrive\kcl",
+    [string]$KCLReleaseJsonUrl = "",
+    [scriptblock]$CustomAssetFactory = $null
+)
+
+Write-Output ""
+$ErrorActionPreference = 'stop'
+
+#Escape space of KCLRoot path
+$KCLRoot = $KCLRoot -replace ' ', '` '
+
+# Constants
+$KCLCliFileName = "kcl.exe"
+$KCLCliFilePath = "${KCLRoot}\${KCLCliFileName}"
+
+# GitHub Org and repo hosting KCL CLI
+$GitHubOrg = "KusionStack"
+$GitHubRepo = "KCLVM"
+
+# Set Github request authentication for basic authentication.
+if ($Env:GITHUB_USER) {
+    $basicAuth = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($Env:GITHUB_USER + ":" + $Env:GITHUB_TOKEN));
+    $githubHeader = @{"Authorization" = "Basic $basicAuth" }
+}
+else {
+    $githubHeader = @{}
+}
+
+if ((Get-ExecutionPolicy) -gt 'RemoteSigned' -or (Get-ExecutionPolicy) -eq 'ByPass') {
+    Write-Output "PowerShell requires an execution policy of 'RemoteSigned'."
+    Write-Output "To make this change please run:"
+    Write-Output "'Set-ExecutionPolicy RemoteSigned -scope CurrentUser'"
+    break
+}
+
+# Change security protocol to support TLS 1.2 / 1.1 / 1.0 - old powershell uses TLS 1.0 as a default protocol
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+# Check if KCL CLI is installed.
+if (Test-Path $KCLCliFilePath -PathType Leaf) {
+    Write-Warning "KCL is detected - $KCLCliFilePath"
+    Invoke-Expression "$KCLCliFilePath --version"
+    Write-Output "Reinstalling KCL..."
+}
+else {
+    Write-Output "Installing KCL..."
+}
+
+# Create KCL Directory
+Write-Output "Creating $KCLRoot directory"
+New-Item -ErrorAction Ignore -Path $KCLRoot -ItemType "directory"
+if (!(Test-Path $KCLRoot -PathType Container)) {
+    throw "Cannot create $KCLRoot, without admin rights"
+}
+
+# Get the list of release from GitHub
+$releaseJsonUrl = $KCLReleaseJsonUrl
+if (!$releaseJsonUrl) {
+    $releaseJsonUrl = "https://api.github.com/repos/${GitHubOrg}/${GitHubRepo}/releases"
+}
+
+$releases = Invoke-RestMethod -Headers $githubHeader -Uri $releaseJsonUrl -Method Get
+if ($releases.Count -eq 0) {
+    throw "No releases from github.com/KusionStack/KCLVM repo"
+}
+
+# get latest or specified version info from releases
+function GetVersionInfo {
+    param (
+        [string]$Version,
+        $Releases
+    )
+    # Filter windows binary and download archive
+    if (!$Version) {
+        $release = $Releases | Where-Object { $_.tag_name -notlike "*rc*" } | Select-Object -First 1
+    }
+    else {
+        $release = $Releases | Where-Object { $_.tag_name -eq "v$Version" } | Select-Object -First 1
+    }
+
+    return $release
+}
+
+# get info about windows asset from release
+function GetWindowsAsset {
+    param (
+        $Release
+    )
+    if ($CustomAssetFactory) {
+        Write-Output "CustomAssetFactory dectected, try to invoke it"
+        return $CustomAssetFactory.Invoke($Release)
+    }
+    else {
+        $windowsAsset = $Release | Select-Object -ExpandProperty assets | Where-Object { $_.name -Like "*windows.zip" }
+        if (!$windowsAsset) {
+            throw "Cannot find the windows KCL CLI binary"
+        }
+        [hashtable]$return = @{}
+        $return.url = $windowsAsset.url
+        $return.name = $windowsAsset.name
+        return $return
+    }`
+}
+
+$release = GetVersionInfo -Version $Version -Releases $releases
+if (!$release) {
+    throw "Cannot find the specified KCL CLI binary version"
+}
+$asset = GetWindowsAsset -Release $release
+$zipFileUrl = $asset.url
+$assetName = $asset.name
+
+$zipFilePath = $KCLRoot + "\" + $assetName
+Write-Output "Downloading $zipFileUrl ..."
+
+$githubHeader.Accept = "application/octet-stream"
+$oldProgressPreference = $progressPreference;
+$progressPreference = 'SilentlyContinue';
+Invoke-WebRequest -Headers $githubHeader -Uri $zipFileUrl -OutFile $zipFilePath
+$progressPreference = $oldProgressPreference;
+if (!(Test-Path $zipFilePath -PathType Leaf)) {
+    throw "Failed to download KCL binary - $zipFilePath"
+}
+
+# Extract KCL CLI to $KCLRoot
+Write-Output "Extracting $zipFilePath..."
+Microsoft.Powershell.Archive\Expand-Archive -Force -Path $zipFilePath -DestinationPath $KCLRoot
+if (!(Test-Path $KCLCliFilePath -PathType Leaf)) {
+    throw "Failed to download KCL archieve - $zipFilePath"
+}
+
+# Check the KCL CLI version
+Invoke-Expression "$KCLCliFilePath -V"
+
+# Clean up zipfile
+Write-Output "Clean up $zipFilePath..."
+Remove-Item $zipFilePath -Force
+
+# Add KCLRoot directory to User Path environment variable
+Write-Output "Try to add $KCLRoot to User Path Environment variable..."
+$UserPathEnvironmentVar = [Environment]::GetEnvironmentVariable("PATH", "User")
+if ($UserPathEnvironmentVar -like '*kcl*') {
+    Write-Output "Skipping to add $KCLRoot to User Path - $UserPathEnvironmentVar"
+}
+else {
+    [System.Environment]::SetEnvironmentVariable("PATH", $UserPathEnvironmentVar + ";$KCLRoot", "User")
+    $UserPathEnvironmentVar = [Environment]::GetEnvironmentVariable("PATH", "User")
+    Write-Output "Added $KCLRoot to User Path - $UserPathEnvironmentVar"
+}
+
+Write-Output "`r`nKCL is installed successfully."
+Write-Output "To get started with KCL, please visit https://kcl-lang.io/docs/user_docs/getting-started/kcl-quick-start ."
+Write-Output "Ensure that Docker Desktop is set to Linux containers mode when you run KCL in self hosted mode."
---
+++ static/script/install.sh
@@ -0,0 +1,226 @@
+#!/usr/bin/env bash
+
+# ------------------------------------------------------------
+# Copyright 2022 The KCL Authors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Reference: https://github.com/dapr/cli/tree/master/install
+# ------------------------------------------------------------
+
+# KCL location
+: ${KCL_INSTALL_DIR:="/usr/local/bin"}
+
+# sudo is required to copy binary to KCL_INSTALL_DIR for linux
+: ${USE_SUDO:="false"}
+
+# Http request CLI
+KCL_HTTP_REQUEST_CLI=curl
+
+# GitHub Organization and repo name to download release
+GITHUB_ORG=KusionStack
+GITHUB_REPO=KCLVM
+
+# KCL filename
+KCL_CLI_FILENAME=kcl
+
+KCL_CLI_FILE="${KCL_INSTALL_DIR}/${KCL_CLI_FILENAME}"
+
+getSystemInfo() {
+    ARCH=$(uname -m)
+    case $ARCH in
+        armv7*) ARCH="arm";;
+        aarch64) ARCH="arm64";;
+        x86_64) ARCH="amd64";;
+    esac
+
+    OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
+
+    # Most linux distro needs root permission to copy the file to /usr/local/bin
+    if [[ "$OS" == "linux" || "$OS" == "darwin" ]] && [ "$KCL_INSTALL_DIR" == "/usr/local/bin" ]; then
+        USE_SUDO="true"
+    fi
+}
+
+verifySupported() {
+    releaseTag=$1
+    local supported=(darwin-amd64 darwin-arm64 linux-amd64 linux-arm linux-arm64)
+    local current_osarch="${OS}-${ARCH}"
+
+    for osarch in "${supported[@]}"; do
+        if [ "$osarch" == "$current_osarch" ]; then
+            echo "Your system is ${OS}_${ARCH}"
+            return
+        fi
+    done
+
+    echo "No prebuilt binary for ${current_osarch}"
+    exit 1
+}
+
+runAsRoot() {
+    local CMD="$*"
+
+    if [ $EUID -ne 0 -a $USE_SUDO = "true" ]; then
+        CMD="sudo $CMD"
+    fi
+
+}
+
+checkHttpRequestCLI() {
+    if type "curl" > /dev/null; then
+        KCL_HTTP_REQUEST_CLI=curl
+    elif type "wget" > /dev/null; then
+        KCL_HTTP_REQUEST_CLI=wget
+    else
+        echo "Either curl or wget is required"
+        exit 1
+    fi
+}
+
+checkExistingKCL() {
+    if [ -f "$KCL_CLI_FILE" ]; then
+        echo -e "\nKCL is detected:"
+        $KCL_CLI_FILE -V
+        echo -e "Reinstalling KCL - ${KCL_CLI_FILE}...\n"
+    else
+        echo -e "Installing KCL...\n"
+    fi
+}
+
+getLatestRelease() {
+    local KCLReleaseUrl="https://api.github.com/repos/${GITHUB_ORG}/${GITHUB_REPO}/releases"
+    local latest_release=""
+
+    if [ "$KCL_HTTP_REQUEST_CLI" == "curl" ]; then
+        latest_release=$(curl -s $KCLReleaseUrl | grep \"tag_name\" | grep -v rc | awk 'NR==1{print $2}' |  sed -n 's/\"\(.*\)\",/\1/p')
+    else
+        latest_release=$(wget -q --header="Accept: application/json" -O - $KCLReleaseUrl | grep \"tag_name\" | grep -v rc | awk 'NR==1{print $2}' |  sed -n 's/\"\(.*\)\",/\1/p')
+    fi
+
+    ret_val=$latest_release
+}
+
+downloadFile() {
+    LATEST_RELEASE_TAG=$1
+
+    KCL_CLI_ARTIFACT="kclvm-${LATEST_RELEASE_TAG}-${OS}-${ARCH}.tar.gz"
+    DOWNLOAD_BASE="https://github.com/${GITHUB_ORG}/${GITHUB_REPO}/releases/download"
+    DOWNLOAD_URL="${DOWNLOAD_BASE}/${LATEST_RELEASE_TAG}/${KCL_CLI_ARTIFACT}"
+
+    # Create the temp directory
+    KCL_TMP_ROOT=$(mktemp -dt kcl-install-XXXXXX)
+    ARTIFACT_TMP_FILE="$KCL_TMP_ROOT/$KCL_CLI_ARTIFACT"
+
+    echo "Downloading $DOWNLOAD_URL ..."
+    if [ "$KCL_HTTP_REQUEST_CLI" == "curl" ]; then
+        curl -SsL "$DOWNLOAD_URL" -o "$ARTIFACT_TMP_FILE"
+    else
+        wget -q -O "$ARTIFACT_TMP_FILE" "$DOWNLOAD_URL"
+    fi
+
+    if [ ! -f "$ARTIFACT_TMP_FILE" ]; then
+        echo "failed to download $DOWNLOAD_URL ..."
+        exit 1
+    fi
+}
+
+isReleaseAvailable() {
+    LATEST_RELEASE_TAG=$1
+
+    KCL_CLI_ARTIFACT="kclvm-${LATEST_RELEASE_TAG}-${OS}-${ARCH}.tar.gz"
+    DOWNLOAD_BASE="https://github.com/${GITHUB_ORG}/${GITHUB_REPO}/releases/download"
+    DOWNLOAD_URL="${DOWNLOAD_BASE}/${LATEST_RELEASE_TAG}/${KCL_CLI_ARTIFACT}"
+
+    if [ "$KCL_HTTP_REQUEST_CLI" == "curl" ]; then
+        httpstatus=$(curl -sSLI -o /dev/null -w "%{http_code}" "$DOWNLOAD_URL")
+        if [ "$httpstatus" == "200" ]; then
+            return 0
+        fi
+    else
+        wget -q --spider "$DOWNLOAD_URL"
+        exitstatus=$?
+        if [ $exitstatus -eq 0 ]; then
+            return 0
+        fi
+    fi
+    return 1
+}
+
+installFile() {
+    tar xf "$ARTIFACT_TMP_FILE" -C "$KCL_TMP_ROOT"
+    local tmp_root_kcl="$KCL_TMP_ROOT/$KCL_CLI_FILENAME"
+
+    if [ ! -f "$tmp_root_kcl" ]; then
+        echo "Failed to unpack KCL executable."
+        exit 1
+    fi
+
+    if [ -f "$KCL_CLI_FILE" ]; then
+        runAsRoot rm "$KCL_CLI_FILE"
+    fi
+    chmod o+x $tmp_root_kcl
+    runAsRoot cp "$tmp_root_kcl" "$KCL_INSTALL_DIR"
+
+    if [ -f "$KCL_CLI_FILE" ]; then
+        echo "$KCL_CLI_FILENAME installed into $KCL_INSTALL_DIR successfully."
+
+        $KCL_CLI_FILE --version
+    else 
+        echo "Failed to install $KCL_CLI_FILENAME"
+        exit 1
+    fi
+}
+
+fail_trap() {
+    result=$?
+    if [ "$result" != "0" ]; then
+        echo "Failed to install KCL"
+        echo "For support, go to https://kcl-lang.io"
+    fi
+    cleanup
+    exit $result
+}
+
+cleanup() {
+    if [[ -d "${KCL_TMP_ROOT:-}" ]]; then
+        rm -rf "$KCL_TMP_ROOT"
+    fi
+}
+
+installCompleted() {
+    echo -e "\nTo get started with KCL, please visit https://kcl-lang.io/docs/user_docs/getting-started/kcl-quick-start"
+}
+
+# -----------------------------------------------------------------------------
+# main
+# -----------------------------------------------------------------------------
+trap "fail_trap" EXIT
+
+getSystemInfo
+checkHttpRequestCLI
+
+if [ -z "$1" ]; then
+    echo "Getting the latest KCL..."
+    getLatestRelease
+else
+    ret_val=v$1
+fi
+
+verifySupported $ret_val
+checkExistingKCL
+
+echo "Installing $ret_val KCL..."
+
+downloadFile $ret_val
+installFile
+cleanup
+
+installCompleted
