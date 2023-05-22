aa614e963bc6ee6d641e2443d743cce2f316d756
Peefy <xpf6677@163.com>
2023-05-22 21:48:36 +0800

    docs: polish data integration documents.

--- examples/test.sh
+++ examples/test.sh
@@ -5,13 +5,9 @@ set -e
 
 pwd=$(cd `dirname $0`; pwd)
 
-# Configuration example tests
-cd $pwd/configuration && make test && cd $pwd
-# Validation example tests
-cd $pwd/validation && make test && cd $pwd
-# Abstraction example tests
-cd $pwd/abstraction && make test && cd $pwd
-# Definition example tests
-cd $pwd/definition && make test && cd $pwd
-# Mutation example tests
-cd $pwd/mutation && make test && cd $pwd
+for path in "configuration" "validation" "abstraction" "definition" "mutation" "data-integration"
+do
+    echo "Testing $path ..."
+    cd $pwd/$path && make test && cd $pwd
+    echo "Test success - $path"
+done
