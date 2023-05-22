2521e704a0fc76840d7a5e79dc91a6b2e3d00144
Peefy <xpf6677@163.com>
2023-05-22 20:16:27 +0800

    tests: add more e2e tests.

--- examples/test.sh
+++ examples/test.sh
@@ -3,7 +3,15 @@
 # Stop on error.
 set -e
 
-pwd=$(cd `dirname $0`/; pwd)
+pwd=$(cd `dirname $0`; pwd)
 
 # Configuration example tests
 cd $pwd/configuration && make test && cd $pwd
+# Validation example tests
+cd $pwd/validation && make test && cd $pwd
+# Abstraction example tests
+cd $pwd/abstraction && make test && cd $pwd
+# Definition example tests
+cd $pwd/definition && make test && cd $pwd
+# Mutation example tests
+cd $pwd/mutation && make test && cd $pwd
