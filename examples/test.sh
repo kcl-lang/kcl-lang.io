201e6ba5ca8cc62090cb811cf3b4cdaef52b0449
cuih <cuihang97@foxmail.com>
2023-05-29 14:08:06 +0800

    fix: examples/test.sh
    Signed-off-by: cuih <cuihang97@foxmail.com>

--- examples/test.sh
+++ examples/test.sh
@@ -3,11 +3,16 @@
 # Stop on error.
 set -e
 
-pwd=$(cd `dirname $0`; pwd)
+pwd=$(
+    cd $(dirname $0)
+    pwd
+)
 
-for path in "configuration" "validation" "abstraction" "definition" "mutation" "data-integration" "automation"
-do
-    echo "Testing $path ..."
-    cd $pwd/$path && make test && cd $pwd
-    echo "Test success - $path"
+for path in "configuration" "validation" "abstraction" "definition" "mutation" "data-integration" "automation"; do
+    echo "\033[1mTesting $path ...\033[0m"
+    if (cd $pwd/$path && make test); then
+        echo "\033[32mTest SUCCESSED - $path\033[0m\n"
+    else
+        echo "\033[31mTest FAILED - $path\033[0m\n"
+    fi
 done
