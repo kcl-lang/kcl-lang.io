eceee4a81555e1035af21a9135910c2a2d9efcc9
peefy <xpf6677@163.com>
2023-06-23 00:44:06 +0800

    feat: add safetyculture logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -165,6 +165,11 @@ function PartnerSection() {
       href: 'https://www.huawei.com/',
     },
     {
+      name: 'SafetyCulture',
+      img: '/img/logos/safetyculture.png',
+      href: 'https://safetyculture.com/',
+    },
+    {
       name: 'TuSimple',
       img: '/img/logos/tusimple.jpg',
       href: 'https://www.tusimple.com/',
